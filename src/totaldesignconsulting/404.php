ef986747764bebbe6d993f179cc1c1377013da18
TDC1Chris <christopher@totaldesignconsulting.com>
2025-06-05 00:39:37 -0400

    updated pages

--- src/totaldesignconsulting/404.php
+++
@@ -1,7 +0,0 @@
-<?php get_header(); ?>
-<main style="text-align: center; padding: 4rem 2rem;">
-    <h1 style="font-size: 2rem; color: #b00;">Page Not Found</h1>
-    <p>We couldn't find what you were looking for.</p>
-    <a href="<?php echo home_url(); ?>" class="button">Return Home</a>
-</main>
-<?php get_footer(); ?>
